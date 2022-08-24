69092fbc9e410778197982b72b87d52a134ca7b5
ReachPooja <reachpoojaprajapat@gmail.com>
2022-08-24 16:57:25 +0530

    update: colors updated

--- lib/src/core/presentation/theme/app_colors.dart
+++ lib/src/core/presentation/theme/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryBackgroundColor = Color(0xfff5ebe0);
-  static const primaryColor = Color(0xffd5bdaf);
+  static const primaryBackgroundColor = Color.fromARGB(255, 255, 226, 230);
+  static const primaryColor = Color(0xff9d8189);
+  static const accentColor = Color(0xfff4acb7);
 }
