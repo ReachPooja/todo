e432b6614b3a3928327bdcdf107040f1f80662ae
ReachPooja <reachpoojaprajapat@gmail.com>
2022-08-30 22:12:46 +0530

    feat: status class

---
+++ lib/src/core/domain/status/status.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:todo/src/core/domain/failures/failures.dart';
+
+part 'status.freezed.dart';
+
+@freezed
+class Status with _$Status {
+  const factory Status.initial() = _Initial;
+  const factory Status.loading() = _Loading;
+  const factory Status.success() = _Success;
+  const factory Status.failure([Failure? failure]) = _StatusFailure;
+}
