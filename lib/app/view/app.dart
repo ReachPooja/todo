0da08f99e17b5c9206126159b5f89a526e924c86
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-01 21:35:51 +0530

    feat: firebase setup

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todo/app/injector/injector.dart';
 import 'package:todo/l10n/l10n.dart';
+import 'package:todo/src/auth/bloc/auth_bloc.dart';
 import 'package:todo/src/auth/view/landing_view.dart';
-import 'package:todo/src/core/presentation/theme/app_theme.dart';
+import 'package:todo/src/core/presentation/styles/app_themes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.ligthTheme,
-      darkTheme: AppTheme.darkTheme,
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const LandingView(),
+    return BlocProvider(
+      create: (context) => getIt<AuthBloc>(),
+      child: MaterialApp(
+        theme: AppTheme.ligthTheme,
+        darkTheme: AppTheme.darkTheme,
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const LandingView(),
+      ),
     );
   }
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:todo/app/app.dart';
+import 'package:todo/app/injector/injector.dart';
 import 'package:todo/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  configureInjector();
+
+  await Firebase.initializeApp();
+
+  await FirebaseAppCheck.instance.activate();
+
+  await bootstrap(() => const App());
 }
