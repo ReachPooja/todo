c6a5b0f83d4248b4369bfbdbad2a74bf4e122c58
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-01 21:33:12 +0530

    feat: spacing

---
+++ lib/src/core/presentation/layout/spacing.dart
@@ -0,0 +1,18 @@
+//* Horizontal Spacing
+import 'package:flutter/material.dart';
+import 'package:todo/src/core/presentation/layout/layout.dart';
+
+const horizontalSpaceTiny = SizedBox(width: Layout.base / 2);
+const horizontalSpaceSmall = SizedBox(width: Layout.base);
+const horizontalSpaceRegular = SizedBox(width: Layout.base * 2);
+const horizontalSpaceMedium = SizedBox(width: Layout.base * 3);
+const horizontalSpaceLarge = SizedBox(width: Layout.base * 4);
+const horizontalSpaceXL = SizedBox(width: Layout.base * 5);
+
+//* Vertical Spacing
+const verticalSpaceTiny = SizedBox(height: Layout.base / 2);
+const verticalSpaceSmall = SizedBox(height: Layout.base);
+const verticalSpaceRegular = SizedBox(height: Layout.base * 2);
+const verticalSpaceMedium = SizedBox(height: Layout.base * 3);
+const verticalSpaceLarge = SizedBox(height: Layout.base * 4);
+const verticalSpaceXL = SizedBox(height: Layout.base * 5);
