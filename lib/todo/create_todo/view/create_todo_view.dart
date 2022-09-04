3820ed4a5e1ce3fba5967ead4cb4a8095d288c05
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-04 21:53:58 +0530

    feat: create todo ui

--- lib/todo/create_todo/view/create_todo_view.dart
+++ lib/todo/create_todo/view/create_todo_view.dart
@@ -1,86 +1,175 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:todo/src/core/presentation/layout/spacing.dart';
+import 'package:todo/src/core/presentation/layout/layout.dart';
+import 'package:todo/src/core/presentation/styles/typography.dart';
 
-class CreateTodoView extends StatelessWidget {
+class CreateTodoView extends StatefulWidget {
   const CreateTodoView({super.key});
 
   @override
+  State<CreateTodoView> createState() => _CreateTodoViewState();
+}
+
+class _CreateTodoViewState extends State<CreateTodoView> {
+  DateTime? selectedDate;
+
+  late TextEditingController _dateTextController;
+  late TextEditingController _categoryTextController;
+
+  @override
+  void initState() {
+    super.initState();
+    _dateTextController = TextEditingController();
+    _categoryTextController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _dateTextController.dispose();
+    _categoryTextController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create todo'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.done),
-          ),
-        ],
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        16,
+        12,
+        16,
+        16 + MediaQuery.of(context).viewInsets.bottom,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Todo',
-                ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Create Task',
+                style: AppTypography.mediumTitleTextStyle,
               ),
-              verticalSpaceRegular,
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Description',
-                ),
-              ),
-              verticalSpaceRegular,
-              TextFormField(
-                readOnly: true,
-                onTap: () async {
-                  final value = await showDateRangePicker(
-                    context: context,
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime.now().add(
-                      const Duration(days: 30),
-                    ),
-                  );
-                  log(value.toString());
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Dates',
-                  suffixIcon: Icon(
-                    Icons.calendar_today_outlined,
-                  ),
-                ),
-              ),
-              verticalSpaceRegular,
-              DropdownButtonFormField<String>(
-                icon: const Icon(
-                  Icons.keyboard_arrow_down_rounded,
-                ),
-                items: List.generate(
-                  status.length,
-                  (index) => DropdownMenuItem(
-                    value: status[index],
-                    child: Text(status[index]),
-                  ),
-                ),
-                onChanged: (value) {},
-                decoration: const InputDecoration(
-                  labelText: 'Status',
-                ),
-              ),
+              CloseButton(),
             ],
           ),
-        ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Todo',
+            ),
+          ),
+          verticalSpaceRegular,
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Description',
+            ),
+          ),
+          verticalSpaceRegular,
+          TextFormField(
+            readOnly: true,
+            controller: _dateTextController,
+            decoration: InputDecoration(
+              labelText: 'Due Date',
+              suffixIcon: DropdownButton<String>(
+                underline: const SizedBox.shrink(),
+                icon: const Icon(
+                  Icons.keyboard_arrow_down,
+                ),
+                items: const [
+                  DropdownMenuItem(
+                    value: 'Today',
+                    child: Text('Today'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Tomorrow',
+                    child: Text('Tomorrow'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Pick a date',
+                    child: Text('Pick a date'),
+                  ),
+                ],
+                onChanged: (value) async {
+                  if (value == 'Pick a date') {
+                    final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(
+                        DateTime.now().year + 1,
+                      ),
+                    );
+                    setState(() {
+                      selectedDate = date;
+                      _dateTextController.text = date!.toIso8601String();
+                    });
+                  }
+                  if (value == 'Today') {
+                    setState(() {
+                      selectedDate = DateTime.now();
+
+                      _dateTextController.text = value!;
+                    });
+                  }
+                  if (value == 'Tomorrow') {
+                    setState(() {
+                      selectedDate = DateTime.now();
+                      _dateTextController.text = value!;
+                    });
+                  }
+                },
+              ),
+            ),
+          ),
+          verticalSpaceRegular,
+          TextFormField(
+            readOnly: true,
+            controller: _categoryTextController,
+            decoration: InputDecoration(
+              labelText: 'Category',
+              suffixIcon: DropdownButton<String>(
+                underline: const SizedBox.shrink(),
+                icon: const Icon(
+                  Icons.keyboard_arrow_down,
+                ),
+                items: const [
+                  DropdownMenuItem(
+                    value: 'All',
+                    child: Text('All'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Personal',
+                    child: Text('Personal'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Home',
+                    child: Text('Home'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Work',
+                    child: Text('Work'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _categoryTextController.text = value!;
+                  });
+                },
+              ),
+            ),
+          ),
+          verticalSpaceRegular,
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add_task_rounded,
+            ),
+            label: const Text(
+              'Add Task',
+              style: AppTypography.mediumBodyTextStyle,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-final status = [
-  'Todo',
-  'In progress',
-  'Done',
-];
