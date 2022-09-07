d4243178d30297f006ce0f99b38ff1f1aeab9658
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-07 18:56:01 +0530

    feat: home ui

--- lib/src/home/view/home_view.dart
+++ lib/src/home/view/home_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:todo/src/auth/auth.dart';
 import 'package:todo/src/core/presentation/layout/layout.dart';
 import 'package:todo/src/core/presentation/styles/styles.dart';
+import 'package:todo/src/home/view/widgets/todo_card.dart';
 import 'package:todo/todo/create_todo/view/create_todo_view.dart';
 
 class HomeView extends StatefulWidget {
@@ -15,6 +16,7 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   late TabController controller;
+  bool isEmpty = false;
 
   @override
   void initState() {
@@ -30,138 +32,107 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    final isLightMode = Theme.of(context).brightness == Brightness.light;
     final user = context.read<AuthBloc>().state.user;
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar(
-            pinned: true,
-            floating: true,
-            title: Text(greeting()),
-            leading: Center(
-              child: CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.white,
-                child: Text(
-                  user.name.isEmpty
-                      ? user.email.substring(0, 1).toUpperCase()
-                      : user.name.substring(0, 1).toUpperCase(),
-                  style: const TextStyle(
-                    color: AppColors.primaryColor,
+          SliverOverlapAbsorber(
+            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+            sliver: SliverAppBar(
+              backgroundColor: Theme.of(context).cardColor,
+              pinned: true,
+              floating: true,
+              forceElevated: innerBoxIsScrolled,
+              title: Text(
+                greeting(),
+                style: TextStyle(
+                  color: isLightMode ? Colors.grey : null,
+                ),
+              ),
+              leading: Center(
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  child: Text(
+                    user.name.isEmpty
+                        ? user.email.substring(0, 1).toUpperCase()
+                        : user.name.substring(0, 1).toUpperCase(),
+                    style: const TextStyle(
+                      color: AppColors.primaryColor,
+                    ),
                   ),
                 ),
               ),
+              bottom: TabBar(
+                labelColor: AppColors.accentColor,
+                unselectedLabelColor: Colors.grey,
+                controller: controller,
+                indicatorWeight: 3,
+                indicatorColor: AppColors.accentColor,
+                labelStyle: AppTypography.mediumBodyTextStyle,
+                tabs: const [
+                  Tab(
+                    text: 'All Tasks',
+                  ),
+                  Tab(
+                    text: 'Personal',
+                  ),
+                  Tab(
+                    text: 'Home',
+                  ),
+                  Tab(
+                    text: 'Work',
+                  ),
+                ],
+              ),
             ),
-            bottom: TabBar(
-              controller: controller,
-              indicatorWeight: 3,
-              indicatorColor: AppColors.accentColor,
-              labelStyle: AppTypography.mediumBodyTextStyle,
-              tabs: const [
-                Tab(
-                  text: 'All Tasks',
-                ),
-                Tab(
-                  text: 'Personal',
-                ),
-                Tab(
-                  text: 'Home',
-                ),
-                Tab(
-                  text: 'Work',
-                ),
-              ],
-            ),
-          )
+          ),
         ],
         body: TabBarView(
           physics: const NeverScrollableScrollPhysics(),
           controller: controller,
           children: [
-            ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              padding: EdgeInsets.zero,
-              itemCount: 8,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Slidable(
-                  key: UniqueKey(),
-                  startActionPane: ActionPane(
-                    motion: const ScrollMotion(),
-                    dismissible: DismissiblePane(
-                      onDismissed: () {},
+            if (isEmpty)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    isLightMode
+                        ? 'assets/images/no_todo.svg'
+                        : 'assets/images/no_todo_dark.svg',
+                    height: MediaQuery.of(context).size.height / 5,
+                  ),
+                  verticalSpaceMedium,
+                  Text(
+                    'No Tasks',
+                    style: AppTypography.mediumTitleTextStyle.copyWith(
+                      color: Colors.grey,
                     ),
-                    children: [
-                      SlidableAction(
-                        flex: 2,
-                        onPressed: (context) {},
-                        backgroundColor: Colors.green,
-                        foregroundColor: Colors.white,
-                        icon: Icons.done,
-                        label: 'Done',
+                  ),
+                ],
+              )
+            else
+              Builder(
+                builder: (context) {
+                  return CustomScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    slivers: [
+                      SliverOverlapInjector(
+                        handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                          context,
+                        ),
+                      ),
+                      SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) => const TodoCard(),
+                          childCount: 7,
+                        ),
                       ),
                     ],
-                  ),
-                  endActionPane: ActionPane(
-                    motion: const ScrollMotion(),
-                    dismissible: DismissiblePane(
-                      onDismissed: () {},
-                    ),
-                    children: [
-                      SlidableAction(
-                        onPressed: (context) {},
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white,
-                        icon: Icons.delete,
-                        label: 'Delete',
-                      ),
-                    ],
-                  ),
-                  child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text(
-                                'Todo name',
-                                style: AppTypography.mediumTitleTextStyle,
-                              ),
-                              Row(
-                                children: const [
-                                  Icon(
-                                    Icons.calendar_today,
-                                    color: AppColors.accentColor,
-                                    size: 18,
-                                  ),
-                                  horizontalSpaceTiny,
-                                  Text(
-                                    '22 Aug',
-                                    style: AppTypography.smallBodyTextStyle,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          verticalSpaceSmall,
-                          const Text(
-                            'Todo description goes here!. And we can we gfed'
-                            ' breifly',
-                          ),
-                          verticalSpaceSmall,
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                  );
+                },
               ),
-            ),
             const Center(
               child: Text('Pending view'),
             ),
